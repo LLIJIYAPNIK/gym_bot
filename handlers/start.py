9ee13b70db612009afbad3c0bf6dd5471638a994
LLIJIYAPNIK <kas4wot@yandex.ru>
2024-06-21 09:53:06 +0300

    Добавлена первая клавиатура

--- handlers/start.py
+++ handlers/start.py
@@ -1,17 +1,22 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from keyboards.inline.start import get_start_kb
+
 
 start_router = Router()
 
 @start_router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Запуск сообщения по команде /start используя фильтр CommandStart()')
+    await message.answer('Запуск сообщения по команде /start используя фильтр CommandStart()', reply_markup=get_start_kb())
 
-@start_router.message(Command('start_2'))
-async def cmd_start_2(message: Message):
-    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()')
 
-@start_router.message(F.text == '/start_3')
-async def cmd_start_3(message: Message):
-    await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!')
+
+
+# @start_router.message(Command('start_2'))
+# async def cmd_start_2(message: Message):
+#     await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()')
+
+# @start_router.message(F.text == '/start_3')
+# async def cmd_start_3(message: Message):
+#     await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!')
---
+++ keyboards/inline/start.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+def get_start_kb():
+  kb_list = [
+		[InlineKeyboardButton(text="Упражнения", callback_data="training")],
+		[InlineKeyboardButton(text="Программы тренировок", callback_data="program")],
+		[InlineKeyboardButton(text="Питание", callback_data="menu")],
+		[InlineKeyboardButton(text="Записаться на тренировку", callback_data="subscribe")]
+	]
+  
+  return InlineKeyboardMarkup(inline_keyboard=kb_list)
